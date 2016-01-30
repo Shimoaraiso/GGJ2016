69500b27387f36105c6fc6d5c2bd3d68fd8d2ac8
fragile <cycad1514@gmail.com>
2016-01-30 12:27:21 +0900

    明かり変更テスト

--- Ritual/Assets/enemy.cs
+++ Ritual/Assets/enemy.cs
@@ -27,7 +27,7 @@ public class enemy : MonoBehaviour {
                 if (transform.position == targetPosition)//目的地に着いたら
                 {
                     Destroy(this.gameObject);//enemy消去
-                    altarState.altarCount++;
+                    altarState.altarCount--;
                 }
                 break;
             case 1://倒された
@@ -46,7 +46,7 @@ public class enemy : MonoBehaviour {
                 if (transform.position == targetPosition)//目的地に着いたら
                 {
                     Destroy(this.gameObject);//enemy消去
-                    altarState.altarCount--;
+                    altarState.altarCount++;
                 }
                 transform.RotateAround(Vector3.zero, new Vector3(0.0f, 0.0f, 1.0f), 120 * Time.deltaTime);//回転の挙動追加
                 break;
