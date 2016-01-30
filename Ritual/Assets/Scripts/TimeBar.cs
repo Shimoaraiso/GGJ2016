57006b77f86592982c7a393f4f37be6960228735
shimoaraiso <wataru.shimoaraiso@ssie.jp>
2016-01-30 19:56:51 +0900

    time bar making

---
+++ Ritual/Assets/Scripts/TimeBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeBar : MonoBehaviour {
+
+	public float barLength = 1.42f;
+	float barSec;
+
+	timer tm;
+
+	void Awake() {
+		tm = GameObject.Find("TimerObject").GetComponent<timer>();
+	}
+
+	void Start () {
+		barSec = barLength / 60;
+	}
+	
+	void Update () {
+
+		transform.localScale = new Vector2(barSec, transform.localScale.y);
+	}
+}
