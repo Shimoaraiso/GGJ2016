2519cb428e44eda2c333b2fb919a8f551c4394b9
fragile <cycad1514@gmail.com>
2016-01-30 15:39:00 +0900

    時間経過でenemy増加

--- Ritual/Assets/main.cs
+++ Ritual/Assets/main.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class main : MonoBehaviour {
 
+    public int playtime;
     public float spawnInterval;
     bool SpaceKey;
     GameObject ESObject;//
     enemySpawn ES;      //enemySpawnスクリプト
     timer tm;
+    int point;
+    int state;
+    bool clear;
 
 
     // Use this for initialization
@@ -17,16 +21,41 @@ public class main : MonoBehaviour {
         ES = ESObject.GetComponent<enemySpawn>(); //EnemySpawnObjectオブジェクトの中のenemySpawnスクリプトを取得
         tm= GameObject.Find("TimerObject").GetComponent<timer>();
         tm.setTime(0.0f);
+        point = playtime / 3;
+         state = 0;
+        clear = false;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-/*
-        if (tm.getTime() > spawnInterval) {
+        if (point <= tm.getPlaytime() &&  point*2> tm.getPlaytime() && state == 0)
+        {
+
+            spawnInterval = spawnInterval / 2.0f;
+            state++;
+        }
+        else if(point * 2<= tm.getPlaytime() && playtime  > tm.getPlaytime() && state == 1)
+        {
+            spawnInterval = spawnInterval / 2.0f;
+            state++;
+            
+        }else if (playtime <= tm.getPlaytime() && state == 2)
+        {
+            if (tm.getPlaytime() >= playtime)
+            {
+                Debug.Log("クリア");
+                clear = true;
+            }
+        }
+
+        if (tm.getTime() > spawnInterval && !clear) {
             ES.enemySpawnRand();//enemyをランダム発生
             tm.setTime(0.0f);
         }
-        */
+        
+        
+        /*
         if (Input.GetKey(KeyCode.A))
         {
             if (!SpaceKey)
@@ -39,7 +68,7 @@ public class main : MonoBehaviour {
         {
             SpaceKey = false;
         }
-        
+        */
 
         
     }
--- Ritual/Assets/timer.cs
+++ Ritual/Assets/timer.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class timer : MonoBehaviour {
 
     float time;
+    float Playtime;
 
     // Use this for initialization
     void Start()
     {
         time = 0.0f;
+        Playtime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
+        Playtime += Time.deltaTime;
     }
 
     /*時間を返す*/
@@ -22,4 +25,10 @@ public class timer : MonoBehaviour {
 
     /*時間をセット*/
     public void setTime(float t) { time = t; }
+
+    /*プレイ時間を返す*/
+    public float getPlaytime() { return Playtime; }
+
+    /*プレイ時間をセット*/
+    public void setPlaytime(float t) { Playtime = t; }
 }
