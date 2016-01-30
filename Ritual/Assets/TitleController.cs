a737ec298283c46d80e84525889c7b8592156afe
shimoaraiso <wataru.shimoaraiso@ssie.jp>
2016-01-30 14:44:40 +0900

    make a Title

---
+++ Ritual/Assets/TitleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class TitleController : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 50, 200, 50), "Stage 1")) {
+			SceneManager.LoadScene("Stage1");
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 110, 200, 50), "Stage 2")) {
+
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 170, 200, 50), "Stage 3")) {
+
+		}
+	}
+}
